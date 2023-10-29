b9f1c85fff3f012d658b1b6ea54fded00affbba3
Tuan Anh Nguyen <ntanh3108@gmail.com>
2023-10-29 22:07:51 +0700

    create route /blog for list of posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return view("post.index", [
+            'posts' => Post::take(5)->get(),
+        ]);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,12 +10,30 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function author(){
+        return $this->belongsTo(User::class, "user_id");
+    }
+
     public function scopePublished($query){
         $query->where('published_at', '<=', Carbon::now());
     }
 
     public function scopeFeatured($query){
         $query->where('featured', true);
+    }
+
+    public function getExcerpt(){
+        return \Illuminate\Support\Str::limit(strip_tags($this->body), 150);
+    }
+
+    public function getMinutes(){
+        $min = round(str_word_count($this->body) / 250);
+
+        return $min<1 ? 1 : $min;
     }
 }
 
